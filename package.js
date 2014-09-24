9b13b3a957002777630e5f822dd16d789426ed7b
Slava Kim <slava@meteor.com>
2014-09-23 22:33:41 -0700

    deps->tracker

--- package.js
+++ package.js
@@ -1,21 +1,20 @@
 Package.describe({
   summary: "Meteor's client-side datastore: a port of Redis to Javascript",
-  version: "1.0.0",
+  version: "1.0.1",
   name: "slava:miniredis",
   git: "https://github.com/meteor/miniredis"
 });
 
 Package.on_use(function (api) {
   api.export('Miniredis');
-  api.use(['id-map', 'deps', 'underscore', 'random', 'ejson']);
+  api.use(['id-map', 'tracker', 'underscore', 'random', 'ejson']);
   api.add_files(['cow.js', 'miniredis.js']);
 });
 
 Package.on_test(function (api) {
-  api.use('miniredis', ['client', 'server']);
+  api.use('slava:miniredis', ['client', 'server']);
   api.use('test-helpers', 'client');
-  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
-           'random', 'deps']);
+  api.use(['tinytest']);
   api.add_files('miniredis-tests.js');
 
   // Usually using Deps on the server is not a good idea
